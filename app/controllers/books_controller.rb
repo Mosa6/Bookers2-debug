29cbe2ed1ef625d0dba0c31f89240b40dc2cfad7
Mosa6 <tikamongi@gmail.com>
2022-04-30 05:47:50 +0000

    [Finish]application3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,11 @@
 class BooksController < ApplicationController
-  
-  before_action :correct_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :destroy, :update]
 
   def show
     @book = Book.find(params[:id])
     @user = @book.user
     @bookl = Book.new
+    @book_comment = BookComment.new
   end
 
   def index
@@ -47,7 +47,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body,)
   end
 
   def correct_user
