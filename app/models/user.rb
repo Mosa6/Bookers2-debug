37d235accf5ab76eb5ac932caf615b319fb82c81
Mosa6 <tikamongi@gmail.com>
2022-05-02 08:09:31 +0000

    [Finish]app5

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,7 @@ class User < ApplicationRecord
     followings.include?(user)
   end
 
+
   def self.looks(search, word)
     if search == "perfect_match"
       @user = User.where("name LIKE?", "#{word}")
