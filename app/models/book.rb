e5cb12a32f71700fe3aaed43cbeb64856a4c8db1
Mosa6 <tikamongi@gmail.com>
2022-05-02 08:43:10 +0000

    [Finish]app5

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,19 +5,17 @@ class Book < ApplicationRecord
   belongs_to :user
   validates :title,presence:true
 
-  def self.looks(search, word)
-    if search == "perfect_match"
-      @book = Book.where("title LIKE?","#{word}")
-    elsif search == "forward_match"
-      @book = Book.where("title LIKE?","#{word}%")
-    elsif search == "backward_match"
-      @book = Book.where("title LIKE?","%#{word}")
-    elsif search == "partial_match"
-      @book = Book.where("title LIKE?","%#{word}%")
-    else
-      @book = Book.all
-    end
-  end
+   def self.search_for(content, method)
+     if method == 'perfect'
+       Book.where(title: content)
+     elsif method == 'forward'
+       Book.where('title LIKE ?', content+'%')
+     elsif method == 'backward'
+       Book.where('title LIKE ?', '%'+content)
+     else
+       Book.where('title LIKE ?', '%'+content+'%')
+     end
+   end
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
