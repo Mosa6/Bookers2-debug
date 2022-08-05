61205ab7b84db4913a946a1f1f6813846eb423ac
Mosa6 <tikamongi@gmail.com>
2022-08-05 11:24:52 +0000

    space

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,7 @@ class Book < ApplicationRecord
   has_many :week_favorites, -> { where(created_at: ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day)) }, class_name: 'Favorite'
   has_many :book_comments, dependent: :destroy
   belongs_to :user
+  
   validates :title,presence:true
 
   scope :created_today, -> { where(created_at: Time.zone.now.all_day) } # 今日
