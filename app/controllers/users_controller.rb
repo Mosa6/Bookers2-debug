90b56e7b7d401380f1dcc5c0fafc49864bbec8e0
Mosa6 <tikamongi@gmail.com>
2022-09-25 05:53:35 +0000

    adjustment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,8 +42,6 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
-
-
   def ensure_correct_user
     @user = User.find(params[:id])
     unless @user == current_user
